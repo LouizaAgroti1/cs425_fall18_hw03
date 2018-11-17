03510b53be568bf48a7ab528ecdf1e0adca594bb
LouizaAgroti1 <31855678+LouizaAgroti1@users.noreply.github.com>
2018-11-17 17:08:11 +0200

    First implementation

---
+++ help.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+ <html>
+   <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Help</title>
+    <meta name="author" content="Louiza Agroti"/>
+    <meta name="description" content="Questions Game - Help"/>
+    <meta name="keywords" content="Questions Game, Help" />
+    <link rel="shortcut icon" href="baseline_map_black_18dp.png" type="image/x-icon" />
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="help.css" />
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+   </head>
+   <body>
+
+    <?php
+        echo "Help!";
+    ?>
+
+   </body>
+ </html>
---
+++ scores.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+ <html>
+   <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Scores</title>
+    <meta name="author" content="Louiza Agroti"/>
+    <meta name="description" content="Questions Game - Scores"/>
+    <meta name="keywords" content="Questions Game, Scores" />
+    <link rel="shortcut icon" href="baseline_map_black_18dp.png" type="image/x-icon" />
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="scores.css" />
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+   </head>
+   <body>
+
+    <?php
+        echo "Scores of the Game!";
+    ?>
+
+   </body>
+ </html>
