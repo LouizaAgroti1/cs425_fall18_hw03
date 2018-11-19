18329aceed547b7852026c1c1b2ad9f76e0fbe90
LouizaAgroti1 <31855678+LouizaAgroti1@users.noreply.github.com>
2018-11-20 00:04:23 +0200

    Implementation of requirements

--- scores.php
+++ scores.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
- <html>
+<html lang="en">
    <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,20 +8,39 @@
     <meta name="description" content="Questions Game - Scores"/>
     <meta name="keywords" content="Questions Game, Scores" />
     <link rel="shortcut icon" href="baseline_map_black_18dp.png" type="image/x-icon" />
-    <!--Import Google Icon Font-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" media="screen" href="scores.css" />
-
-    <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
    </head>
    <body>
 
-    <?php
-        echo "Scores of the Game!";
-    ?>
+    <div class="navbar">
+        <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="scores.php">Scores</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="help.php">Help</a>
+        </li>
+        </ul>
+    </div>
+
+    <br>
+    <br>
+
+    <div class="title">
+        <h1>High Scores</h1>
+    </div>
+
+    <div class="fixed-bottom">
+        <footer class="footer">
+            <h4>Good luck!!!</h4>
+        </footer>
+    </div>
 
    </body>
  </html>
