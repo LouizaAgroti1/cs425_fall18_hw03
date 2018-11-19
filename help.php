b57841e98fe131cf1f25c366046837b0d56ba216
LouizaAgroti1 <31855678+LouizaAgroti1@users.noreply.github.com>
2018-11-20 00:03:52 +0200

    Implementation of requirements and its operation

--- help.php
+++ help.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
- <html>
+<html lang="en">
    <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,20 +8,53 @@
     <meta name="description" content="Questions Game - Help"/>
     <meta name="keywords" content="Questions Game, Help" />
     <link rel="shortcut icon" href="baseline_map_black_18dp.png" type="image/x-icon" />
-    <!--Import Google Icon Font-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" media="screen" href="help.css" />
-
-    <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
    </head>
    <body>
 
-    <?php
-        echo "Help!";
-    ?>
+    <div class="navbar">
+        <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="scores.php">Scores</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="help.php">Help</a>
+        </li>
+        </ul>
+    </div>
 
+    <br>
+    <br>
+
+     
+    <div class="title">
+      <h1>Instructions</h1>
+    </div>
+
+    <br>
+    <br>
+
+    <div class="container">
+      <p>The questions are 10 and they have three levels of difficulty. The score for each question is based on the level of difficulty.
+        The difficulty of the next question is selected based on the current question’s answer. If you answer correctly,
+        the next question would be from a higher level of difficulty. If you answer wrong, then the next one would be easier.
+        The first question would be of medium difficulty. During the game, you would know how many questions you answered
+        so far and how many are left. At the end of the game, you would see an overall score of your performance
+        that will show the list of the questions, if you answered correctly or not, the difficulty, the score for each answer
+        and your overall score. You also have the option to save your score using a nickname.</p> 
+    </div>
+
+    <div class="fixed-bottom">
+        <footer class="footer">
+            <h4>Good luck!!!</h4>
+        </footer>
+    </div>
+    
    </body>
  </html>
